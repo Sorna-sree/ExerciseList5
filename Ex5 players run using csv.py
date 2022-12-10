b8ff43cd337b57b9589fc5c1e41d867f7a851cba
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-12-10 18:38:47 +0530

    Add files via upload

--- Ex5 players run using csv.py
+++ Ex5 players run using csv.py
@@ -3,29 +3,30 @@ data=[]
 runs_list=[]
 
 
-file=open("C:\\Users\\sornasree\\Desktop\\index.csv","r")
+file=open("C:\\Users\\sornasree\\Desktop\\playerscore.csv","r")
 file=csv.reader(file)
 next(file) #remove the first row
+print(file)
 
 #store the csv file information in list
 for value in file:
     data.append(value)
 print(data)
 
-#store the runs in list
-for runs in range(0,len(data)):
-    runs_list.append(data[runs][5])
+for value in range(0,len(data)):
+    runs_list.append(data[value][5])
+
+#string is converted to the integer
+runs_list=[int(i) for i in runs_list]
 print(runs_list)
 
+#print(len(runs_list))
 
-#sorting the list
-runs_list.sort()
-#runs_list.sort(key=int)
-
-print(runs_list)
-
-
-print("first winner: ",runs_list[-1])
-print("second winner: ",runs_list[-2])
+#print the differents of the two players
+for index in range(0,len(runs_list),1):
+    player1=(runs_list[index])-(runs_list[index+1])
+    player2=(runs_list[index+1])-(runs_list[index+2])
+    print("player1: ",player1)
+    print("player2: ",player2)
 
 
