230d568a43f5ab2debe171e0a3a16c4b1d90b5fc
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-12-10 20:16:55 +0530

    Add files via upload

---
+++ Ex5  counting players country using csv.py
@@ -0,0 +1,37 @@
+import csv
+data=[]
+player_name=[]
+Indcount=0
+Engcount=0
+Auscount=0
+
+file=open("C:\\Users\\sornasree\\Desktop\\playerscore.csv","r")
+file=csv.reader(file)
+next(file) #remove the first row
+print(file)
+
+#store the csv file information in list
+for value in file:
+    data.append(value)
+print(data)
+
+#store the player name in a list
+for value in range(0,len(data)):
+    player_name.append(data[value][0])
+print(player_name)
+
+#calculate the number of players from each country
+for value in range(0,len(player_name)):
+    if "IND" in player_name[value]:
+        Indcount+=1
+    
+    if "ENG" in player_name[value]:
+        Engcount+=1
+    
+    if "AUS" in player_name[value]:
+        Auscount+=1
+        
+        
+print("Indian player count is: ",Indcount)
+print("England player count is: ",Auscount)
+print("Australia player count is: ",Engcount)
